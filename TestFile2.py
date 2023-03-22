564764af939037eba28e661a74318ec119f33bba
AlexToumayan <108097689+AlexToumayan@users.noreply.github.com>
2023-03-22 14:42:01 -0700

    Update TestFile2.py

--- TestFile2.py
+++ TestFile2.py
@@ -1,70 +1,121 @@
-import os
+"""""
+Chat GPT Flashcards To Anki Converter
+
+Created by Alex Toumayan, a self study project created at UC Berkeley during 2023.
+Copyright (c) 2023 Alex Toumayan. All rights reserved.
+"""
+
 import re
-import tempfile
-import zipfile
-from tkinter import *
-from tkinter import filedialog
-from genanki import Deck, Model, Note, Package
+import genanki
+import os
+from tkinter import Tk, Label, Button, Text, filedialog, messagebox, Menu
+from tkinter.ttk import Frame
 
-MODEL = Model(
-    1607392319,
-    "Simple Model",
-    fields=[
-        {"name": "Front"},
-        {"name": "Back"},
-    ],
-    templates=[
-        {
-            "name": "Card 1",
-            "qfmt": "{{Front}}",
-            "afmt": "{{Front}}<hr id='answer'>{{Back}}",
-        },
-    ],
-)
+def parse_flashcards(content):
+    flashcards_raw = re.findall(r'(?i)(?:front(?:\s+of\s+card)?\s*:\s*(.+?)(?:\s*back(?:\s+of\s+card)?\s*:\s*|(?=front(?:\s+of\s+card)?\s*:)))(?:back(?:\s+of\s+card)?\s*:\s*(.+?)(?=\s*front(?:\s+of\s+card)?\s*:|$))?', content, re.DOTALL)
 
-def parse_flashcards(text):
     flashcards = []
-    for line in text.split('\n'):
-        if ':' in line:
-            front, back = [s.strip() for s in line.split(':', 1)]
-            if front and back:
-                flashcards.append((front, back))
+    for front, back in flashcards_raw:
+        front = front.strip()
+        back = back.strip()
+        flashcards.append((front, back))
+
     return flashcards
 
-def create_anki_deck(flashcards, deck_name):
-    deck = Deck(deck_id=os.urandom(8).hex(), name=deck_name)
+def process_text(content, window):
+   flashcards = parse_flashcards(content)
+
+   if not flashcards:
+       messagebox.showerror("Error", "Invalid format. Please follow the instructions and try again.", parent=window)
+       return
+
+   output_file = filedialog.asksaveasfilename(parent=window, defaultextension=".apkg", title="Save Anki Package")
+   if not output_file:
+       return
+
+   deck_name = os.path.splitext(os.path.basename(output_file))[0]
+   export_to_apkg(flashcards, output_file, deck_name)
+
+def export_to_apkg(flashcards, output_file, deck_name):
+    model = genanki.Model(
+        1607392319,
+        'Simple Model',
+        fields=[
+            {'name': 'Front'},
+            {'name': 'Back'},
+        ],
+        templates=[
+            {
+                'name': 'Card 1',
+                'qfmt': '{{Front}}',
+                'afmt': '{{FrontSide}}<hr id="answer">{{Back}}',
+            },
+        ])
+
+    deck = genanki.Deck(
+        2059400110,
+        deck_name)
+
     for front, back in flashcards:
-        note = Note(model=MODEL, fields=[front, back])
+        note = genanki.Note(
+            model=model,
+            fields=[front, back])
         deck.add_note(note)
-    return deck
 
-def export_deck(deck, filename):
-    package = Package(deck)
-    package.write_to_file(filename)
+    genanki.Package(deck).write_to_file(output_file)
 
-def on_submit():
-    text = input_text.get(1.0, END).strip()
-    flashcards = parse_flashcards(text)
-    if not flashcards:
-        status_label.config(text="Please enter flashcards in the correct format.")
-        return
-    filename = filedialog.asksaveasfilename(defaultextension=".apkg", filetypes=[("Anki Package", "*.apkg")])
-    if not filename:
-        return
-    deck_name = os.path.splitext(os.path.basename(filename))[0]
-    deck = create_anki_deck(flashcards, deck_name)
-    export_deck(deck, filename)
-    status_label.config(text="Flashcards exported successfully!")
+def create_gui():
+   window = Tk()
+   window.title('Chat GPT Flashcards To Anki Converter')
 
-root = Tk()
-root.title("Flashcard Generator")
 
-Label(root, text="Enter Flashcards (Front: Back)").grid(row=0, column=0, padx=5, pady=5, sticky=W)
-input_text = Text(root, wrap=WORD, width=60, height=10)
-input_text.grid(row=0, column=1, padx=5, pady=5, sticky=W)
+   frame = Frame(window)
+   frame.pack(fill="both", expand=True)
 
-Button(root, text="Submit", command=on_submit).grid(row=1, column=1, padx=5, pady=5, sticky=E)
-status_label = Label(root, text="")
-status_label.grid(row=2, column=1, padx=5, pady=5, sticky=W)
 
-root.mainloop()
+   Label(frame, text='Paste your text here:').pack(side="top", anchor="w", padx=10, pady=10)
+
+
+   text_widget = Text(frame, wrap='word', width=60, height=20)
+   text_widget.pack(fill="both", expand=True, padx=10, pady=10)
+
+
+   # Create the right-click context menu for the text widget
+   context_menu = Menu(text_widget, tearoff=0)
+   context_menu.add_command(label="Copy", command=lambda: text_widget.event_generate("<<Copy>>"))
+   context_menu.add_command(label="Paste", command=lambda: text_widget.event_generate("<<Paste>>"))
+
+
+   def show_context_menu(event):
+       # macOS uses Button-2 for right-click
+       if event.num == 3 or (event.num == 2 and window.tk.call("tk", "windowingsystem") == "aqua"):
+           context_menu.tk_popup(event.x_root, event.y_root)
+
+
+   text_widget.bind("<Button-2>", show_context_menu)
+   text_widget.bind("<Button-3>", show_context_menu)
+
+
+   Button(frame, text='Convert', command=lambda: process_text(text_widget.get(1.0, "end-1c"), window)).pack(side="bottom", pady=10)
+
+
+   instructions = (
+       "Instructions:\n\n"
+       "1. Use the following format for your text:\n"
+       "   Front: Your question\n"
+       "   Back: Your answer\n\n"
+       "2. You can also use 'Front of card:' and 'Back of card:' as labels.\n\n"
+       "Example:\n\n"
+       "   Front of card: What is the capital of France?\n"
+       "   Back of card: Paris"
+   )
+
+
+   Label(frame, text=instructions, justify="left").pack(side="bottom", anchor="w", padx=10, pady=10)
+
+
+   window.mainloop()
+
+
+if __name__ == '__main__':
+   create_gui()
